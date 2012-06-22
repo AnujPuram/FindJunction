90539cdf338f60f7029d5f9aba12d0cae2e39096
AnujPuram <apuram@uncc.edu>
2012-06-22 12:05:25 -0400

    FindJunctionOperator Class further optimised

--- src/findjunction/FindJunctionOperator.java
+++ src/findjunction/FindJunctionOperator.java
@@ -6,6 +6,7 @@ package findjunction;
 
 import com.affymetrix.genometryImpl.BioSeq;
 import com.affymetrix.genometryImpl.SeqSpan;
+import com.affymetrix.genometryImpl.filter.SymmetryFilterI;
 import com.affymetrix.genometryImpl.operator.Operator;
 import com.affymetrix.genometryImpl.parsers.FileTypeCategory;
 import com.affymetrix.genometryImpl.symmetry.*;
@@ -21,6 +22,10 @@ import java.util.*;
  */
 public class FindJunctionOperator implements Operator{
     private final int default_threshold = 5;
+    private static SymmetryFilterI noIntronFilter = new NoIntronFilter();
+    private static SymmetryFilterI childThresholdFilter = new ChildThresholdFilter();
+    
+    
     int threshold = default_threshold;
     
     @Override
@@ -35,54 +40,23 @@ public class FindJunctionOperator implements Operator{
 
     @Override
     public SeqSymmetry operate(BioSeq bioseq, List<SeqSymmetry> list) {
-        int count;
-        NoIntronFilter noIntronFilter = new NoIntronFilter();
-        ChildThresholdFilter childFilter = new ChildThresholdFilter();
-        List<SeqSymmetry> filteredIntrons = new ArrayList<SeqSymmetry>(); 
-        SeqSymmetry currentIntron;
-        for(SeqSymmetry sym : list){
-            if(noIntronFilter.filterSymmetry(bioseq, sym)){
-                List<SeqSymmetry> splitFilteredIntrons = split(sym , bioseq);
-                filteredIntrons.addAll(splitFilteredIntrons);
-            }
-        }
-        TypeContainerAnnot container = createJunctions(filteredIntrons, bioseq);
-        return container;
-    }
-    
-    public TypeContainerAnnot createJunctions(List<SeqSymmetry> filteredIntrons, BioSeq bioseq){
         TypeContainerAnnot container = new TypeContainerAnnot("test", "bed");
         HashMap<String, SpecificUcscBedSym> map = new HashMap<String , SpecificUcscBedSym>();
-        for(SeqSymmetry intronSym : filteredIntrons){
-            addToMap(intronSym, map, bioseq);
+        
+        for(SeqSymmetry sym : list){
+            if(noIntronFilter.filterSymmetry(bioseq, sym)){
+                updateIntronHashMap(sym , bioseq, map);
+            }
         }
-        Set<String> keySet = map.keySet();
-        Object keys[] = keySet.toArray();
-        for(int i=0;i<keys.length;i++){
-            container.addChild(map.get((String)keys[i]));
+        Collection<SpecificUcscBedSym> symmetrySet = map.values();
+        Object syms[] = symmetrySet.toArray();
+        for(int i=0;i<syms.length;i++){
+            container.addChild((SpecificUcscBedSym)syms[i]);
         }
         return container;
     }
-    
-    public void addToMap(SeqSymmetry intronSym , HashMap<String, SpecificUcscBedSym> map, BioSeq bioseq){
-        int blockMins[] = new int[2];
-        int blockMaxs[] = new int[2];
-        FindJunction junction = new FindJunction();
-        SeqSpan span = intronSym.getSpan(bioseq);
-        blockMins[0] = span.getMin() - junction.getThreshold();
-        blockMins[1] = span.getMax();
-        blockMaxs[0] = span.getMin();
-        blockMaxs[1] = span.getMax() + junction.getThreshold();
-        String name = "J:"+bioseq.getID()+":"+span.getMin()+"-"+span.getMax();
-        SpecificUcscBedSym tempSym = new SpecificUcscBedSym("test", bioseq, span.getMin()-junction.getThreshold(),
-            span.getMax()+junction.getThreshold(), name, 1, true, 0, 0, blockMins, blockMaxs);
-        if(map.containsKey(name)){
-            float score = ((SpecificUcscBedSym)(map.get(name))).getScore();
-            map.get(name).setScore(++score);                    
-        }
-        else
-            map.put(name,tempSym);
-    }
+        
+
     
     @Override
     public int getOperandCountMin(FileTypeCategory ftc) {
@@ -119,29 +93,43 @@ public class FindJunctionOperator implements Operator{
     }
     
     //This method splits the given Sym into introns and filters out the qualified Introns
-    public List<SeqSymmetry> split(SeqSymmetry sym , BioSeq bioseq){
-        List<SeqSymmetry> splitFilteredIntrons = new ArrayList<SeqSymmetry>();
+    private static void updateIntronHashMap(SeqSymmetry sym , BioSeq bioseq, HashMap<String, SpecificUcscBedSym> map){
         List<Integer> childIntronIndices = new ArrayList<Integer>();
         int childCount = sym.getChildCount();
-        SeqSymmetry intronChild;
-        ChildThresholdFilter filter = new ChildThresholdFilter();
+        SeqSymmetry intronChild, intronSym;
+    
         for(int i=0;i<childCount - 1;i++){
-            if(filter.filterSymmetry(bioseq, sym.getChild(i)) && filter.filterSymmetry(bioseq, sym.getChild(i+1))){
+            if(childThresholdFilter.filterSymmetry(bioseq, sym.getChild(i)) && childThresholdFilter.filterSymmetry(bioseq, sym.getChild(i+1))){
                 childIntronIndices.add(i);
             }
         }
-        if(childIntronIndices.size() > 0 && childIntronIndices.size() != (sym.getChildCount()-1)){
+        if(childIntronIndices.size() > 0){
+            intronSym = SeqUtils.getIntronSym(sym, bioseq);
             for(Integer i : childIntronIndices){
-                SeqSymmetry currentIntron = SeqUtils.getIntronSym(sym, bioseq).getChild(i);
-                splitFilteredIntrons.add(currentIntron);
+                intronChild = intronSym.getChild(i);
+                addToMap(intronChild, map, bioseq);
             }
         }
-        else if(childIntronIndices.size() == (sym.getChildCount() - 1)){
-            SeqSymmetry intronSym = SeqUtils.getIntronSym(sym, bioseq);
-            for(int i=0;i<intronSym.getChildCount() ;i++)
-                splitFilteredIntrons.add(intronSym.getChild(i));
+    }
+    
+    private static void addToMap(SeqSymmetry intronSym , HashMap<String, SpecificUcscBedSym> map, BioSeq bioseq){
+        int blockMins[] = new int[2];
+        int blockMaxs[] = new int[2];
+        FindJunction junction = new FindJunction();
+        SeqSpan span = intronSym.getSpan(bioseq);
+        blockMins[0] = span.getMin() - junction.getThreshold();
+        blockMins[1] = span.getMax();
+        blockMaxs[0] = span.getMin();
+        blockMaxs[1] = span.getMax() + junction.getThreshold();
+        String name = "J:"+bioseq.getID()+":"+span.getMin()+"-"+span.getMax();
+        SpecificUcscBedSym tempSym = new SpecificUcscBedSym("test", bioseq, span.getMin()-junction.getThreshold(),
+            span.getMax()+junction.getThreshold(), name, 1, true, 0, 0, blockMins, blockMaxs);
+        if(map.containsKey(name)){
+            float score = ((SpecificUcscBedSym)(map.get(name))).getScore();
+            map.get(name).setScore(++score);                    
         }
-        return splitFilteredIntrons;
+        else
+            map.put(name,tempSym);
     }
     
 }
