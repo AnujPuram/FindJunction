5a782bd340cc54a526e6d372ec575117b9e9762d
Hiral Vora <hiralv@gmail.com>
2012-07-23 12:12:15 -0400

    Adding sample test.

---
+++ test/SampleTest.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author hiralv
+ */
+public class SampleTest {
+    
+    @Before
+    public void setUp(){
+        
+    }
+    
+    @Test
+    public void sample(){
+        assertEquals(1,1);
+    }
+    
+    @After
+    public void end(){
+        
+    }
+}
